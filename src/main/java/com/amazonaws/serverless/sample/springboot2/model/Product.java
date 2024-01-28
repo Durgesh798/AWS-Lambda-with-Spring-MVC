93117b80e8fec25df2fe824e0e7acba7073ef307
Durgesh798 <durgeshwarreddy.perolla@gmail.com>
2024-01-27 23:19:14 -0600

    intial commit

---
+++ src/main/java/com/amazonaws/serverless/sample/springboot2/model/Product.java
@@ -0,0 +1,32 @@
+package com.amazonaws.serverless.sample.springboot2.model;
+
+public class Product {
+
+    String pid;
+    String name;
+    String price;
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
